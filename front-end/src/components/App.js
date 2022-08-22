c4303336047fff5f2832e2e078051083cf53c0b4
santannaizadora <santannaizadora@gmail.com>
2022-08-22 12:54:29 -0300

    test

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(async () => {
     const API_URL = process.env.REACT_APP_BASE_URL;
     try {
-      const response = await axios.get(`${API_URL}students/random`);
+      const response = await axios.get(`${API_URL}/students/random`);
       const student = response.data;
       if(!student){
         alert("Putz! Não há estudantes cadastrados para o sorteio!");
